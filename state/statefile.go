4d3018a83c481e94d0b33925cea5e3526cf441c6
joonas.fi <pompo500@xs.fi>
2017-06-10 14:32:33 +0300

    Statefile encryption

--- state/statefile.go
+++ state/statefile.go
@@ -1,9 +1,14 @@
 package state
 
 import (
+	"crypto/rand"
+	"crypto/sha256"
 	"encoding/json"
 	"github.com/pquerna/otp"
 	"github.com/pquerna/otp/totp"
+	"golang.org/x/crypto/nacl/secretbox"
+	"golang.org/x/crypto/pbkdf2"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -157,15 +162,52 @@ type Statefile struct {
 	Folders []Folder
 }
 
+func passwordTo256BitEncryptionKey(pwd string, salt []byte) [32]byte {
+	started := time.Now()
+
+	// 1.4sec @ 100k on Raspberry Pi 2
+	// https://github.com/borgbackup/borg/issues/77#issuecomment-130459726
+	iterationCount := 100 * 1000
+	encryptionKey := pbkdf2.Key(
+		[]byte(pwd),
+		salt,
+		iterationCount,
+		32,
+		sha256.New)
+
+	if len(encryptionKey) != 32 {
+		panic("returned pbkdf2 key not 32 bytes")
+	}
+
+	var ret [32]byte
+	copy(ret[:], encryptionKey)
+
+	log.Printf("Encryption key derived from password in %s", time.Since(started))
+
+	return ret
+}
+
 func (s *Statefile) Save() {
 	jsonBytes, errJson := json.MarshalIndent(s, "", "    ")
 	if errJson != nil {
 		panic(errJson)
 	}
 
-	err := ioutil.WriteFile(statefilePath, jsonBytes, 0644)
+	// You must use a different nonce for each message you encrypt with the
+	// same key. Since the nonce here is 192 bits long, a random value
+	// provides a sufficiently small probability of repeats.
+	var nonce [24]byte
+	if _, err := io.ReadFull(rand.Reader, nonce[:]); err != nil {
+		panic(err)
+	}
 
-	if err != nil {
+	// using Seal() nonce as PBKDF2 salt
+	encryptionKey := passwordTo256BitEncryptionKey("hunter2", nonce[:])
+
+	encryptedBytes := []byte{}
+	encryptedBytes = secretbox.Seal(nonce[:], jsonBytes, &nonce, &encryptionKey)
+
+	if err := ioutil.WriteFile(statefilePath, encryptedBytes, 0644); err != nil {
 		panic(err)
 	}
 }
@@ -200,9 +242,25 @@ func ReadStatefile() (*Statefile, error) {
 		writeBlankStatefile()
 	}
 
-	jsonBytes, err := ioutil.ReadFile(statefilePath)
+	encryptedBytes, err := ioutil.ReadFile(statefilePath)
 	if err != nil {
 		panic(err)
+	}
+
+	// When you decrypt, you must use the same nonce and key you used to
+	// encrypt the message. One way to achieve this is to store the nonce
+	// alongside the encrypted message. Above, we stored the nonce in the first
+	// 24 bytes of the encrypted text.
+	// 24 bytes of nonce seems fine https://security.stackexchange.com/a/112592
+	var decryptNonce [24]byte
+	copy(decryptNonce[:], encryptedBytes[:24])
+
+	// using Seal() nonce as PBKDF2 salt
+	secretKeyBytes := passwordTo256BitEncryptionKey("hunter2", decryptNonce[:])
+
+	jsonBytes, ok := secretbox.Open(nil, encryptedBytes[24:], &decryptNonce, &secretKeyBytes)
+	if !ok {
+		panic("decryption error. wrong password?")
 	}
 
 	if err := json.Unmarshal(jsonBytes, &s); err != nil {
