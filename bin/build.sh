280df00f2aafb895331decbe2039c18465477a4d
Joonas Loppi <joonas@joonas.fi>
2017-12-17 18:54:03 +0200

    Start using Travis + Bintray

---
+++ bin/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -eu
+
+logBuild () {
+	target="$1"
+
+	echo "# Building $target"
+}
+
+rm -rf rel
+mkdir rel
+
+# go generate
+
+echo "# Building rel/public.tar.gz"
+
+tar -czf rel/public.tar.gz public/
+
+logBuild "linux-arm"
+
+GOOS=linux GOARCH=arm go build -o rel/pism_linux-arm
+
+logBuild "linux-amd64"
+
+GOOS=linux GOARCH=amd64 go build -o rel/pism_linux-amd64
+
+# the CLI breaks automation unless opt-out..
+export JFROG_CLI_OFFER_CONFIG=false
+
+jfrog-cli bt upload \
+	"--user=joonas" \
+	"--key=$BINTRAY_APIKEY" \
+	--publish=true \
+	'rel/*' \
+	"function61/pi-security-module/main/$FRIENDLY_REV_ID" \
+	"$FRIENDLY_REV_ID/"
---
+++ bin/ci-build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -eu
+
+# "3811ac3f4de838f51f877d55d74881ccb431d4b0" => "3811ac3f4de838f5"
+CI_REVISION_ID_SHORT=${CI_REVISION_ID:0:16}
+
+# "20171217_1632_3811ac3f4de838f5"
+FRIENDLY_REV_ID="$(date +%Y%m%d_%H%M)_$CI_REVISION_ID_SHORT"
+
+# use (almost) empty directory as a build context for the build image
+mkdir -p empty_dir
+
+cp Dockerfile.build empty_dir/Dockerfile.build
+
+docker build -f empty_dir/Dockerfile.build -t bob empty_dir/
+
+docker run \
+	--rm \
+	-it \
+	-v "$(pwd):/app" \
+	-e "CI_REVISION_ID=$CI_REVISION_ID" \
+	-e "FRIENDLY_REV_ID=$FRIENDLY_REV_ID" \
+	-e "BINTRAY_APIKEY=$BINTRAY_APIKEY" \
+	bob
