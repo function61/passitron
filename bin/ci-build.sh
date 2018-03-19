c8e5d5f42586455a8e457c53afe9df4e245dbb65
Joonas Loppi <joonas@joonas.fi>
2018-03-19 17:37:58 +0200

    Make the build image less specific to our project

--- bin/ci-build.sh
+++ bin/ci-build.sh
@@ -6,18 +6,29 @@ CI_REVISION_ID_SHORT=${CI_REVISION_ID:0:16}
 # "20171217_1632_3811ac3f4de838f5"
 FRIENDLY_REV_ID="$(date +%Y%m%d_%H%M)_$CI_REVISION_ID_SHORT"
 
-# use (almost) empty directory as a build context for the build image
-mkdir -p empty_dir
+docker_run_contextless_build () {
+	local dockerfile="$1"
+	local image_name="$2"
 
-cp Dockerfile.build empty_dir/Dockerfile.build
+	# Docker *requires* a directory that is used for build context. with the
+	# build image we don't need any, so we'll create an empty directory for it.
+	# IIRC it needs to be a subdirectory of our current working directory.
+	mkdir -p empty_dir
 
-docker build -f empty_dir/Dockerfile.build -t bob empty_dir/
+	cp "$dockerfile" "empty_dir/${dockerfile}"
+
+	docker build -f "empty_dir/${dockerfile}" -t "$image_name" empty_dir/
+}
+
+docker_run_contextless_build "Dockerfile.build" "pism-builder"
 
 docker run \
 	--rm \
 	-it \
+	--workdir "/go/src/github.com/function61/pi-security-module" \
 	-v "$(pwd):/go/src/github.com/function61/pi-security-module" \
 	-e "CI_REVISION_ID=$CI_REVISION_ID" \
 	-e "FRIENDLY_REV_ID=$FRIENDLY_REV_ID" \
 	-e "BINTRAY_APIKEY=$BINTRAY_APIKEY" \
-	bob
+	pism-builder \
+	bin/build.sh
