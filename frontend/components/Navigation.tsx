3c60f727cc47e1cc4f842e5e39817e572937deb5
Joonas Loppi <joonas@joonas.fi>
2018-05-14 20:50:30 +0300

    Highlight active section in Navigation

--- frontend/components/Navigation.tsx
+++ frontend/components/Navigation.tsx
@@ -4,20 +4,24 @@ import {auditlogRoute, indexRoute, settingsRoute, sshkeysRoute} from 'routes';
 interface NavLink {
 	url: string;
 	title: string;
+	active: boolean;
 }
 
 export default class Navigation extends React.Component<{}, {}> {
 	render() {
+		const hash = document.location.hash;
+
 		const links: NavLink[] = [
-			{ title: 'Home', url: indexRoute.buildUrl({}) },
-			{ title: 'SSH keys', url: sshkeysRoute.buildUrl({}) },
-			{ title: 'Settings', url: settingsRoute.buildUrl({}) },
-			{ title: 'Audit log', url: auditlogRoute.buildUrl({}) },
+			{ title: 'Home', url: indexRoute.buildUrl({}), active: indexRoute.matchUrl(hash) !== null },
+			{ title: 'SSH keys', url: sshkeysRoute.buildUrl({}), active: sshkeysRoute.matchUrl(hash) !== null },
+			{ title: 'Settings', url: settingsRoute.buildUrl({}), active: settingsRoute.matchUrl(hash) !== null },
+			{ title: 'Audit log', url: auditlogRoute.buildUrl({}), active: auditlogRoute.matchUrl(hash) !== null },
 		];
 
-		const items = links.map((link: NavLink) => {
-			// TODO: <li class="active">
-			return <li key={link.url}><a href={link.url}>{link.title}</a></li>;
+		const items = links.map((link) => {
+			const activeOrNot = link.active ? 'active' : '';
+
+			return <li className={activeOrNot} key={link.url}><a href={link.url}>{link.title}</a></li>;
 		});
 
 		return <ul className="nav nav-tabs">{items}</ul>;
