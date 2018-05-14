ca7b86f18af612d766a39cf7c3c09bb273fb44cb
Joonas Loppi <joonas@joonas.fi>
2018-05-14 20:52:24 +0300

    dropdown: support label

--- frontend/components/dropdown.tsx
+++ frontend/components/dropdown.tsx
@@ -1,17 +1,21 @@
 import * as React from 'react';
 
 interface DropdownProps {
+	label?: string;
 	children: JSX.Element[];
 }
 
 export class Dropdown extends React.Component<DropdownProps, {}> {
 	render() {
+		const maybeLabel = this.props.label ? this.props.label + ' ' : '';
+
 		const items = this.props.children.map((child, idx) => {
 			return <li key={idx}>{child}</li>;
 		});
 
 		return <div className="btn-group">
 			<button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				{maybeLabel}
 				<span className="caret"></span>
 			</button>
 			<ul className="dropdown-menu">
