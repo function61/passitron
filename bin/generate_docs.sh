1cb249a35ac638f8c938af000e2245fd203d2537
Joonas Loppi <joonas@joonas.fi>
2018-06-01 23:54:30 +0300

    Build automatic documentation of internal dependencies

--- bin/generate_docs.sh
+++ bin/generate_docs.sh
@@ -5,6 +5,14 @@ go generate ./...
 export LC_ALL="C.UTF-8"
 export LANG="C.UTF-8"
 
+# internal dependencies
+
+echo "\`\`\`" > docs/internal-dependencies.md
+(cd cmd/pism && depth . | grep github.com/function61/pi-security-module/ | grep -v vendor) >> docs/internal-dependencies.md
+echo "\`\`\`" >> docs/internal-dependencies.md
+
+# mkdocs
+
 rm -rf docs_ready/
 cd docs/
 mkdocs build --clean -f mkdocs.yml -d ../docs_ready
