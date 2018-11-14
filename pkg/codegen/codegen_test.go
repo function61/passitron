191242a546a716d830f459c2b29a533ceddb65dc
Joonas Loppi <joonas@joonas.fi>
2018-11-14 17:36:58 +0200

    eventhorizon/util/ass -> gokit/assert

--- pkg/codegen/codegen_test.go
+++ pkg/codegen/codegen_test.go
@@ -1,13 +1,13 @@
 package codegen
 
 import (
-	"github.com/function61/eventhorizon/util/ass"
+	"github.com/function61/gokit/assert"
 	"testing"
 )
 
 func TestBeginsWithUppercaseLetter(t *testing.T) {
-	ass.True(t, isCustomType("Foo"))
-	ass.False(t, isCustomType("foo"))
+	assert.True(t, isCustomType("Foo"))
+	assert.True(t, !isCustomType("foo"))
 
-	ass.False(t, isCustomType("!perkele"))
+	assert.True(t, !isCustomType("!perkele"))
 }
--- pkg/state/eventhandlers_test.go
+++ pkg/state/eventhandlers_test.go
@@ -1,7 +1,7 @@
 package state
 
 import (
-	"github.com/function61/eventhorizon/util/ass"
+	"github.com/function61/gokit/assert"
 	"github.com/function61/pi-security-module/pkg/domain"
 	"testing"
 	"time"
@@ -19,9 +19,9 @@ func TestScenario(t *testing.T) {
 	otpToken(t, st)
 
 	waccs := st.WrappedAccountsByFolder(domain.RootFolderId)
-	ass.EqualInt(t, len(waccs), 1)
+	assert.True(t, len(waccs) == 1)
 
-	ass.EqualString(t, waccs[0].Account.Id, "acc1")
+	assert.EqualString(t, waccs[0].Account.Id, "acc1")
 }
 
 func meta() domain.EventMeta {
@@ -36,13 +36,15 @@ func firstAccountCreated(t *testing.T, st *State) {
 	event(st, domain.NewAccountCreated("acc1", domain.RootFolderId, "Example account", meta()))
 	event(st, domain.NewAccountUsernameChanged("acc1", "AzureDiamond", meta()))
 	event(st, domain.NewAccountDescriptionChanged("acc1", "my cool account", meta()))
+	event(st, domain.NewAccountUrlChanged("acc1", "https://www.example.com/path", meta()))
 
 	wacc := st.WrappedAccountById("acc1")
 
-	ass.EqualString(t, wacc.Account.Id, "acc1")
-	ass.EqualString(t, wacc.Account.Title, "Example account")
-	ass.EqualString(t, wacc.Account.Username, "AzureDiamond")
-	ass.EqualString(t, wacc.Account.Description, "my cool account")
+	assert.EqualString(t, wacc.Account.Id, "acc1")
+	assert.EqualString(t, wacc.Account.Title, "Example account")
+	assert.EqualString(t, wacc.Account.Username, "AzureDiamond")
+	assert.EqualString(t, wacc.Account.Description, "my cool account")
+	assert.EqualString(t, wacc.Account.Url, "https://www.example.com/path")
 }
 
 func addPassword(t *testing.T, st *State) {
@@ -50,11 +52,11 @@ func addPassword(t *testing.T, st *State) {
 
 	wacc := st.WrappedAccountById("acc1")
 
-	ass.EqualInt(t, len(wacc.Secrets), 1)
+	assert.True(t, len(wacc.Secrets) == 1)
 
-	ass.EqualString(t, wacc.Secrets[0].Secret.Id, "sec1")
-	ass.EqualString(t, string(wacc.Secrets[0].Secret.Kind), domain.SecretKindPassword)
-	ass.EqualString(t, wacc.Secrets[0].Secret.Password, "hunter2")
+	assert.EqualString(t, wacc.Secrets[0].Secret.Id, "sec1")
+	assert.EqualString(t, string(wacc.Secrets[0].Secret.Kind), domain.SecretKindPassword)
+	assert.EqualString(t, wacc.Secrets[0].Secret.Password, "hunter2")
 }
 
 func addKeylist(t *testing.T, st *State) {
@@ -68,18 +70,18 @@ func addKeylist(t *testing.T, st *State) {
 
 	wacc := st.WrappedAccountById("acc1")
 
-	ass.EqualInt(t, len(wacc.Secrets), 2)
+	assert.True(t, len(wacc.Secrets) == 2)
 
 	wsecret := wacc.Secrets[1]
 
 	assertKeyItem := func(idx int, key string, value string) {
-		ass.EqualString(t, wsecret.KeylistKeys[idx].Key, key)
-		ass.EqualString(t, wsecret.KeylistKeys[idx].Value, value)
+		assert.EqualString(t, wsecret.KeylistKeys[idx].Key, key)
+		assert.EqualString(t, wsecret.KeylistKeys[idx].Value, value)
 	}
 
-	ass.EqualString(t, wsecret.Secret.Id, "sec2")
-	ass.EqualString(t, string(wsecret.Secret.Kind), domain.SecretKindKeylist)
-	ass.EqualInt(t, len(wsecret.KeylistKeys), 3)
+	assert.EqualString(t, wsecret.Secret.Id, "sec2")
+	assert.EqualString(t, string(wsecret.Secret.Kind), domain.SecretKindKeylist)
+	assert.True(t, len(wsecret.KeylistKeys) == 3)
 	assertKeyItem(0, "01", "9765")
 	assertKeyItem(1, "02", "8421")
 	assertKeyItem(2, "03", "1298")
@@ -87,35 +89,35 @@ func addKeylist(t *testing.T, st *State) {
 
 func renameAccount(t *testing.T, st *State) {
 	// before rename
-	ass.EqualString(t, st.WrappedAccountById("acc1").Account.Title, "Example account")
+	assert.EqualString(t, st.WrappedAccountById("acc1").Account.Title, "Example account")
 
 	event(st, domain.NewAccountRenamed("acc1", "Renamed example account", meta()))
 
-	ass.EqualString(t, st.WrappedAccountById("acc1").Account.Title, "Renamed example account")
+	assert.EqualString(t, st.WrappedAccountById("acc1").Account.Title, "Renamed example account")
 }
 
 func deleteAccount(t *testing.T, st *State) {
-	ass.EqualInt(t, len(st.WrappedAccountsByFolder(domain.RootFolderId)), 1)
+	assert.True(t, len(st.WrappedAccountsByFolder(domain.RootFolderId)) == 1)
 
 	event(st, domain.NewAccountCreated("acc2", domain.RootFolderId, "Example account", meta()))
 
-	ass.EqualInt(t, len(st.WrappedAccountsByFolder(domain.RootFolderId)), 2)
+	assert.True(t, len(st.WrappedAccountsByFolder(domain.RootFolderId)) == 2)
 
 	event(st, domain.NewAccountDeleted("acc2", meta()))
 
-	ass.EqualInt(t, len(st.WrappedAccountsByFolder(domain.RootFolderId)), 1)
+	assert.True(t, len(st.WrappedAccountsByFolder(domain.RootFolderId)) == 1)
 }
 
 func deleteKeylist(t *testing.T, st *State) {
-	ass.EqualInt(t, len(st.WrappedAccountById("acc1").Secrets), 2)
+	assert.True(t, len(st.WrappedAccountById("acc1").Secrets) == 2)
 
 	event(st, domain.NewAccountSecretDeleted("acc1", "sec2", meta()))
 
-	ass.EqualInt(t, len(st.WrappedAccountById("acc1").Secrets), 1)
+	assert.True(t, len(st.WrappedAccountById("acc1").Secrets) == 1)
 }
 
 func otpToken(t *testing.T, st *State) {
-	ass.EqualInt(t, len(st.WrappedAccountById("acc1").Secrets), 1)
+	assert.True(t, len(st.WrappedAccountById("acc1").Secrets) == 1)
 
 	event(st, domain.NewAccountOtpTokenAdded(
 		"acc1",
@@ -123,9 +125,9 @@ func otpToken(t *testing.T, st *State) {
 		"otpauth://totp/Google%3Afoo%40example.com?secret=qlt6vmy6svfx4bt4rpmisaiyol6hihca&issuer=Google",
 		meta()))
 
-	ass.EqualInt(t, len(st.WrappedAccountById("acc1").Secrets), 2)
+	assert.True(t, len(st.WrappedAccountById("acc1").Secrets) == 2)
 
-	ass.EqualString(
+	assert.EqualString(
 		t,
 		st.WrappedAccountById("acc1").Secrets[1].OtpProvisioningUrl,
 		"otpauth://totp/Google%3Afoo%40example.com?secret=qlt6vmy6svfx4bt4rpmisaiyol6hihca&issuer=Google")
