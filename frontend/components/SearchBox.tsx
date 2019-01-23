9b5c30d9618218118dac465becbb67d0809b6f75
Joonas Loppi <joonas@joonas.fi>
2019-01-23 15:50:29 +0200

    use react-autocomplete for quicker navigation to search results

--- frontend/components/SearchBox.tsx
+++ frontend/components/SearchBox.tsx
@@ -1,32 +1,99 @@
+import {defaultErrorHandler} from 'backenderrors';
+import {search} from 'generated/restapi';
 import * as React from 'react';
+import * as Autocomplete from 'react-autocomplete';
 import {indexRoute, searchRoute} from 'routes';
+import {accountRoute, folderRoute} from 'routes';
 
 interface SearchBoxProps {
-	searchTerm?: string;
+	searchTerm?: string; // initial
 }
 
-export class SearchBox extends React.Component<SearchBoxProps, {}> {
+interface SearchBoxState {
+	searchTerm?: string;
+	items: AutocompleteItem[];
+	hasInflight: boolean;
+}
+
+interface AutocompleteItem {
+	label: string;
+	url: string;
+}
+
+export class SearchBox extends React.Component<SearchBoxProps, SearchBoxState> {
+	state: SearchBoxState = { hasInflight: false, items: [] };
+	private hasInflight = false;
+	private queuedQuery: undefined | string;
+
 	render() {
-		return <input
-			type="text"
-			style={{width: '250px'}}
-			className="form-control"
-			defaultValue={this.props.searchTerm}
-			onKeyPress={(e) => { this.onSubmit(e); }}
-			placeholder="Search .." />;
+		return <Autocomplete
+			inputProps={{onKeyPress: (e: any) => { // https://github.com/reactjs/react-autocomplete/issues/338
+				if (e.key !== 'Enter') {
+					return;
+				}
+
+				// => user hit enter with non-suggested term => go to search results
+				const searchTerm = e.target.value;
+
+				if (searchTerm !== '') {
+					this.navigate(searchRoute.buildUrl({searchTerm}));
+				} else {
+					this.navigate(indexRoute.buildUrl({}));
+				}
+			}}}
+			getItemValue={ (item: AutocompleteItem) => item.url }
+			items={this.state.items}
+			renderItem={(item: AutocompleteItem, isHighlighted: boolean) =>
+				<div style={{ background: isHighlighted ? 'lightgray' : 'white' }} key={item.url}>
+					{item.label}
+				</div>
+			}
+			value={this.state.searchTerm}
+			onChange={(e: any) => { this.setState({ searchTerm: e.target.value, items: [] }); this.searchtermChanged(e.target.value); }}
+			onSelect={(url: string) => { this.navigate(url); }}
+		/>;
 	}
 
-	onSubmit(e: React.KeyboardEvent<HTMLInputElement>) {
-		if (e.charCode !== 13) {
+	private searchtermChanged(term: string) {
+		if (this.hasInflight) {
+			this.queuedQuery = term;
 			return;
 		}
 
-		const searchTerm = e.currentTarget.value;
+		this.hasInflight = true;
 
-		if (searchTerm !== '') {
-			document.location.hash = searchRoute.buildUrl({searchTerm});
-		} else {
-			document.location.hash = indexRoute.buildUrl({});
-		}
+		const scheduleNextOk = () => {
+			setTimeout(() => {
+				this.hasInflight = false;
+				const qq = this.queuedQuery;
+				this.queuedQuery = undefined;
+				if (qq !== undefined) {
+					this.searchtermChanged(qq);
+				}
+			}, 1000);
+		};
+
+		search(term).then((resp) => {
+			const folderMatches: AutocompleteItem[] = resp.SubFolders.map((item) => ({
+				label: item.Name,
+				url: folderRoute.buildUrl({ folderId: item.Id }),
+			}));
+
+			const accountMatches: AutocompleteItem[] = resp.Accounts.map((item) => ({
+				label: item.Title,
+				url: accountRoute.buildUrl({ id: item.Id }),
+			}));
+
+			this.setState({ items: folderMatches.concat(accountMatches) });
+
+			scheduleNextOk();
+		}, (err) => {
+			scheduleNextOk();
+			defaultErrorHandler(err);
+		});
+	}
+
+	private navigate(url: string) {
+		document.location.hash = url;
 	}
 }
--- frontend/modules.d.ts
+++ frontend/modules.d.ts
@@ -1,6 +1,8 @@
 // https://stackoverflow.com/a/12695001
 
+// badly behaving external modules with no TypeScript defs
 declare module 'jsqrcode';
+declare module 'react-autocomplete';
 
 // FIXME: type this shit out
 declare var u2f: any;
