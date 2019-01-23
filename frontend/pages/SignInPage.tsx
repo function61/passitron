d59b94ca23cbce0536b29facd01ca02545be2bd6
Joonas Loppi <joonas@joonas.fi>
2019-01-23 13:21:12 +0200

    ask username and password separately, so on re-login we can only ask for the password

--- frontend/pages/SignInPage.tsx
+++ frontend/pages/SignInPage.tsx
@@ -14,7 +14,8 @@ import {shouldAlwaysSucceed, unrecognizedValue} from 'utils';
 
 enum UnauthenticatedKind {
 	Sealed, // while database is sealed, signing in is not possible
-	NotSignedIn,
+	AwaitingUsername,
+	AwaitingPassword,
 }
 
 interface SignInPageProps {
@@ -22,10 +23,14 @@ interface SignInPageProps {
 }
 
 interface SignInPageState {
-	status: UnauthenticatedKind;
+	status?: UnauthenticatedKind;
+	username: string;
 }
 
 export default class SignInPage extends React.Component<SignInPageProps, SignInPageState> {
+	state: SignInPageState = {
+		username: localStorage.getItem('signInLastUsername') || '',
+	};
 	private title = 'Sign in';
 
 	componentDidMount() {
@@ -33,14 +38,12 @@ export default class SignInPage extends React.Component<SignInPageProps, SignInP
 	}
 
 	render() {
-		let content = <Loading />;
-
-		if (this.state) {
-			content = this.widgetByStatus(this.state.status);
-		}
+		const widget = this.state.status !== undefined ?
+			this.widgetByStatus(this.state.status) :
+			<Loading />;
 
 		return <DefaultLayout title={this.title} breadcrumbs={this.getBreadcrumbs()}>
-			{content}
+			{widget}
 		</DefaultLayout>;
 	}
 
@@ -52,15 +55,30 @@ export default class SignInPage extends React.Component<SignInPageProps, SignInP
 
 				<CommandInlineForm command={DatabaseUnseal()} />
 			</Panel>;
-		case UnauthenticatedKind.NotSignedIn:
-			return <Panel heading="Sign in">
-				<WarningAlert text="You need to sign in." />
-
-				<CommandInlineForm command={SessionSignIn()} />
+		case UnauthenticatedKind.AwaitingUsername:
+			return <Panel heading="Username">
+				<form onSubmit={() => { this.usernameEntered(); }}>
+				<label>
+					Username:
+					<input type="text" value={this.state.username} onChange={(e) => { this.setState({username: e.target.value}); }} />
+				</label>
+					<input type="submit" value="Submit" />
+				</form>
+			</Panel>;
+		case UnauthenticatedKind.AwaitingPassword:
+			return <Panel heading={'Username: ' + this.state.username}>
+				<CommandInlineForm command={SessionSignIn(this.state.username)} />
 			</Panel>;
 		default:
 			return unrecognizedValue(status);
 		}
+	}
+
+	private usernameEntered() {
+		// store, so next on next login we can pre-fill this
+		localStorage.setItem('signInLastUsername', this.state.username);
+
+		this.setState({status: UnauthenticatedKind.AwaitingPassword});
 	}
 
 	private getBreadcrumbs(): Breadcrumb[] {
@@ -92,7 +110,9 @@ export default class SignInPage extends React.Component<SignInPageProps, SignInP
 			if (isSealedError(ser)) {
 				return UnauthenticatedKind.Sealed;
 			} else if (isNotSignedInError(ser)) {
-				return UnauthenticatedKind.NotSignedIn;
+				return this.state.username === '' ?
+					UnauthenticatedKind.AwaitingUsername :
+					UnauthenticatedKind.AwaitingPassword;
 			}
 
 			throw ex; // some other error - shouldn't happen
