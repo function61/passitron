661f5fa1470f4002625380c81265d48865527e0d
Joonas Loppi <joonas@joonas.fi>
2019-02-14 13:26:13 +0200

    f61ui: add notranslate to increase privacy

--- pkg/f61ui/f61ui.go
+++ pkg/f61ui/f61ui.go
@@ -31,6 +31,7 @@ const template = `<!doctype html>
 <html>
 <head>
 	<meta charset="UTF-8" />
+	<meta name="google" content="notranslate" />
 	<title></title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css" />
